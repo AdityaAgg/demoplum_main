d7df254a8a02cea66a7edd7b9551f9899b7513f1
AdityaAgg <agga140@usc.edu>
2018-03-08 01:17:32 -0800

    start generates response

--- src/controllers/session/start.js
+++ src/controllers/session/start.js
@@ -1,65 +1,67 @@
 const validate = require('express-validation')
 const sessionValidation = require('../../validators/session/session')
-
+const session = require('express-session')
 
 module.exports = (router, app, db) => {
 
   router.post('/start',
   validate(sessionValidation),
   async (req, res, next) => {
-
     //recreate the session
-    req.session.regenerate(function(err) {
+    req.session.regenerate(async function(err) {
       console.log(err);
-    });
 
-    //create session data object
-    let params = ["appId", "deviceId",  "apiVersion", "devMode", "createDisposition", "appVersion", "userId", "systemName", "location", "userAttributes"]
-
-
-    //create the session data
-    let sessDataObj = params=>map((param) => {
-      let obj = {};
-      obj[param] = req.body[param];
-      return obj;
-    }, {});
-
-    req.session.obj = sessDataObj;
+      console.log("reached here");
+      //create session data object
+      let params = ["appId", "deviceId",  "apiVersion", "devMode", "createDisposition", "appVersion", "userId", "systemName", "location", "userAttributes"]
 
 
+      //create the session data
+      let sessDataObj = {};
+      params.forEach(param => {
+        sessDataObj[param] = req.body[param];
+      });
 
-    let resArray = [];
-    let user = await db.User.findOne({userId: req.session.obj.userId});
-    if(user) {
-      if(createDisposition == "CreateIfNeeded") //if create disposition is create if needed create user
-      {
-        let user = new db.User(Object.assign({}, req.session.obj));
-        await user.save(); //save user
+      req.session.obj = sessDataObj;
+      console.log(req.session.obj)
+
+
+      let resArray = [];
+      let user = await db.User.findOne({userId: req.session.obj.userId});
+      if(!user) {
+        console.log(req.session.obj["createDisposition"]);
+        if(req.session.obj["createDisposition"] == "CreateIfNeeded") //if create disposition is create if needed create user
+        {
+          console.log("user lookup failed")
+          let user = new db.User(Object.assign({}, req.session.obj));
+          await user.save(); //save user
+          let resObj = {};
+          resObj.success = user.toJSON();
+          resArray.push(resObj);
+          res.status(200).json(resArray);
+        }
+        else
+        {
+          let resObj = {warning: {message: 'user does not exist and create disposition is set to "CreateIfNeeded"'}};
+          resArray.push(resObj);
+          res.status(200).json(resArray);
+        }
+      } else {
+        params.forEach((param) => {
+          if (req.session.obj[param]) {
+            user.set(param, req.session.obj[param])
+          }
+        });
+
+        await user.save();
+
         let resObj = {};
         resObj.success = user.toJSON();
         resArray.push(resObj);
         res.status(200).json(resArray);
       }
-      else
-      {
-        let resObj = {warning: {message: 'user does not exist and create disposition is set to "CreateIfNeeded"'}};
-        resArray.push(resObj);
-        res.status(200).json(resArray);
-      }
-    } else {
-      params.forEach((param) => {
-        if (req.session.obj[param]) {
-          user.set(param, req.session.obj[param])
-        }
-      });
+    });
 
-      await user.save();
-
-      let resObj = {};
-      resObj.success = user.toJSON();
-      resArray.push(resObj);
-      res.status(200).json(resArray);
-    }
   })
 
 
